bb6e2dcf476648b7383cfc08d61b69b4ce4349e7
ibra-kdbra <sonarfarouq@gmail.com>
2023-04-16 00:17:05 +0300

    pages folder

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+    <h1 className='text-green-300 underline'>
+      Hello my G</h1>
+    </>
+  )
+}
