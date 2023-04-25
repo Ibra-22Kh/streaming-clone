fc06affa87f6a662a8f023f175184e5417bd5f31
ibra-kdbra <sonarfarouq@gmail.com>
2023-04-25 22:27:56 +0300

    pages folder modified

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -1,33 +1,39 @@
-import bycrpt from 'bcrypt';
+import bcrypt from 'bcrypt';
 import { NextApiRequest, NextApiResponse } from 'next';
-import prismadb from "@/lib/prismadb";
+import prismadb from '@/libs/prismadb';
 
-export default async function handler(req:NextApiRequest, res:NextApiResponse) {
-    if(req.method !== 'POST'){
-        return res.status(405).end();
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    if (req.method !== 'POST') {
+      return res.status(405).end();
     }
-    try {
-        const {email,name,password} = req.body;
 
-        const exisitingUser = await prismadb.user.findUnique({
-            where:{
-                email,
-            }
-        })
-        if(exisitingUser){
-            return res.status(422).json({error:'Email taken'})
-        }
-        const hashedPassword = await bycrpt.hash(password,12);
-        const user = await prismadb.user.create({
-            data:{
-                email,name,hashedPassword,
-                image:'',
-                emailVerified: new Date(),
-            }
-        })
-        return res.status(200).json(user);
-    } catch (error) {
-        console.log(error);
-        return res.status(400).end();
+    const { email, name, password } = req.body;
+
+    const existingUser = await prismadb.user.findUnique({
+      where: {
+        email
+      }
+    })
+
+    if (existingUser) {
+      return res.status(422).json({ error: 'Email taken' });
     }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = await prismadb.user.create({
+      data: {
+        email,
+        name,
+        hashedPassword,
+        image: '',
+        emailVerified: new Date(),
+      }
+    })
+
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(400).json({ error: `Something went wrong: ${error}` });
+  }
 }
